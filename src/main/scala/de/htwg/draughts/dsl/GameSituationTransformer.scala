af235a314c968bf905d0e5767ef20a5ddd5b754a
Simon Mennig <simon.mennig@hotmail.de>
2019-07-01 11:38:55 +0200

    Added validation for field color

--- src/main/scala/de/htwg/draughts/dsl/GameSituationTransformer.scala
+++ src/main/scala/de/htwg/draughts/dsl/GameSituationTransformer.scala
@@ -1,5 +1,7 @@
 package de.htwg.draughts.dsl
 
+import de.htwg.draughts.model.Colour
+
 object GameSituationTransformer {
 
   def apply(parseResult: Either[String, GameSituation]):
@@ -8,25 +10,27 @@ object GameSituationTransformer {
       parsed <- parseResult
       players <- checkPlayers(parsed)
       fields <- checkFields(players)
-        } yield {
+    } yield {
       fields
     }
 
-  def checkPlayers(gameSituation:  GameSituation):Either[String, GameSituation] =
+  def checkPlayers(gameSituation: GameSituation): Either[String, GameSituation] =
 
-    if(gameSituation.players._1.color == gameSituation.players._2.color){
+    if (gameSituation.players._1.color == gameSituation.players._2.color) {
       Left("Players can not have the same color")
-    }else{
+    } else {
       Right(gameSituation)
     }
 
-  def checkFields(gameSituation:  GameSituation):Either[String, GameSituation] =
-    if (gameSituation.fields.map(f => (f.column,f.row)).distinct.size == gameSituation.fields.size){
+  def checkFields(gameSituation: GameSituation): Either[String, GameSituation] =
+    if (gameSituation.fields.exists(f => f.getColour == Colour.WHITE))
+      Left(s"Only black fields are allowed to have pieces!\n" +
+        s"Following Fileds are not allowed: ${gameSituation.fields.filter(f => f.getColour == Colour.WHITE)}")
+    else if (gameSituation.fields.map(f => (f.column, f.row)).distinct.size == gameSituation.fields.size) {
       Right(gameSituation)
-    }else{
+    } else {
       Left("Some fields are set more then once!")
     }
-
 
 
 }
